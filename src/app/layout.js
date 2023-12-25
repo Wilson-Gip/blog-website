2c71180d05b9077b492c79a15cc1e215c22829fb
Wilson-Gip <wilsongip101@gmail.com>
2023-12-25 03:23:54 -0800

    styling and responsive design implemented

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,11 @@
+import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
+//change for seo
 export const metadata = {
   title: 'Blog App',
   description: 'The best blog app!',
@@ -11,7 +14,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">
+            <Navbar/>
+            {children}
+            <Footer/>
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
