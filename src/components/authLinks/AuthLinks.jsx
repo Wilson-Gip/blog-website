6324b725f1773f2e3beb19edd189a5208bc2567c
Wilson-Gip <wilsongip101@gmail.com>
2023-12-27 08:47:30 -0800

    added authentication and google oauth client id login and logout

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -2,9 +2,12 @@
 import Link from "next/link";
 import styles from "./authLinks.module.css";
 import { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const AuthLinks = () => {
   const [open, setOpen] = useState(false);
+
+  const { status } = useSession();
 
   return (
     <>
@@ -17,6 +20,9 @@ const AuthLinks = () => {
           <Link href="/write" className={styles.link}>
             Write
           </Link>
+          <span className={styles.link} onClick={signOut}>
+            Logout
+          </span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
