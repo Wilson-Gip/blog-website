736da854b24c5fc95d6d18b0d91ef13084bf36f9
Wilson-Gip <wilsongip101@gmail.com>
2023-12-27 18:02:18 -0800

    Update Menu.jsx

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./menu.module.css";
 import Link from "next/link";
 import Image from "next/image";
-import MenuPosts from "@/compoonents/menuPosts/menuPosts";
+// import MenuPosts from "@/compoonents/menuPosts/menuPosts";
 // import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
@@ -10,13 +10,13 @@ const Menu = () => {
     <div className={styles.container}>
       <h2 className={styles.subtitle}>{"What's hot"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
-      <MenuPosts withImage={false} />
+      {/* <MenuPosts withImage={false} /> */}
       <h2 className={styles.subtitle}>Discover by topic</h2>
       {/* <h1 className={styles.title}>Categories</h1>
       <MenuCategories /> */}
       <h2 className={styles.subtitle}>Chosen by the editor</h2>
       <h1 className={styles.title}>Editors Pick</h1>
-      <MenuPosts withImage={true} />
+      {/* <MenuPosts withImage={true} /> */}
     </div>
   );
 };
